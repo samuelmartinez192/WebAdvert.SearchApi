fce8f1c740119c80bf867318f0ac4210b5241af4
Samuel Martinez <samuel.martinez192@outlook.com>
2020-05-18 15:23:57 -0600

    Add project files.

---
+++ Extensions/AddNestConfigurationExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System;
+
+using WebAdvert.SearchApi.Models;
+
+namespace WebAdvert.SearchApi.Extensions
+{
+    public static class AddNestConfigurationExtension
+    {
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            string elasticSearchUrl = configuration.GetSection("ES").GetValue<string>("url");
+
+            var connectionSettings = new ConnectionSettings(new Uri(elasticSearchUrl))
+                .DefaultIndex("advert")
+                .DefaultMappingFor<AdvertType>(advert => advert.IdProperty(p => p.Id));
+
+            var client = new ElasticClient(connectionSettings);
+
+            services.AddSingleton<IElasticClient>(client);
+        }
+    }
+}
